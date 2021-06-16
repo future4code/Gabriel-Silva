466094e9f3edfa34897d8af5e8538e731590a596
unknown <gbldja@gmail.com>
2021-06-15 23:19:09 -0300

    Resolução do exercício 2 escrita de código)

--- quinzena2/funcoes-de-array/index.js
+++ quinzena2/funcoes-de-array/index.js
@@ -99,3 +99,81 @@ const premiacaoPoodle = filtrarPoodle.map((inputPoodle) => {
 })
 console.log(premiacaoPoodle)
 
+
+// ------------------- EXERCÍCIO 2 ------------------- // 
+console.log("\n")
+console.log("Exercício 2\n")
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+]
+console.log(produtos)
+// a) Crie um novo array que contenha apenas o nome de cada item
+console.log("\na) Array com nome dos itens:")
+
+const arrayNomeItens = produtos.filter((inputItens) => {
+    return inputItens.nome
+})
+const produtosFiltrados = arrayNomeItens.map((produtos)=>{
+    return produtos.nome
+})
+console.log(produtosFiltrados)
+
+//b) Crie um novo array que contenha um objeto com o nome e o preço de cada item, aplicando 5% de desconto em todos eles
+
+// Separei os produtos e seus valores, mas não consegui criar uma nova array de objetos somente com os dados solicitados
+// Não consegui avançar a partir do código abaixo: 
+
+// console.log("\nb) Nome e preço de itens com 5% de desconto:")
+// const nomeItens = produtos.map((inputProduto)=>{
+//     return inputProduto.nome 
+// })
+// console.log(nomeItens)
+
+// const precoItens = produtos.map((inputProduto)=>{
+//     const desconto5porc = inputProduto.preco - (inputProduto.preco * 0.05)
+//     return desconto5porc.toFixed(2)
+// })
+// console.log(precoItens)
+
+//Com a ajuda do colega Ryan, solucionamos o problema da seguinte maneira:
+const arrayItemComDesconto = produtos.map((inputItens) =>{
+    return {nome: inputItens.nome, preco: 0.05*inputItens.preco.toFixed(2)}
+})
+console.log(arrayItemComDesconto)
+
+
+//)c) Crie um novo array que contenha apenas os objetos da categoria Bebidas 
+console.log("\nd) Array somente com as bebidas:")
+const filtrarBebidas = produtos.filter ((inputBebidas) => {
+    return inputBebidas.categoria === "Bebidas"
+})
+console.log(filtrarBebidas)
+
+//d) Crie um novo array que contenha apenas os objetos cujo nome contenha a palavra "Ypê"
+console.log("\nd) Array de objetos apenas com a palavra Ypê")
+let listaProdutos = produtos.filter ((inputProduto)=>{
+    return inputProduto.nome.includes("Ypê")
+})
+console.log(listaProdutos)
+
+//e) Crie um novo array onde cada item é uma frase "Compre [NOME] por [PREÇO]". 
+// Esse array deve conter frases apenas dos itens cujo nome contenha a palavra "Ypê"
+
+let listaProdutos2 = produtos.filter ((inputProduto)=>{
+    return inputProduto.nome.includes("Ypê")
+})
+
+let listaProdutos3 = listaProdutos2.map((inputProduto)=>{
+    return `Compre ${inputProduto.nome} por preço ${inputProduto.preco}`
+})
+console.log(listaProdutos3)
