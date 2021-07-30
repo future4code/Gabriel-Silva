2a1e51b567e4db797b093a8fd38cc72e42c90036
Gabazvedo <gbldja@gmail.com>
2021-07-29 23:08:51 -0300

    Entrega exercício 22

--- quinzena6/boiler-plate/src/App.js
+++ quinzena6/boiler-plate/src/App.js
@@ -1,74 +1,104 @@
-import React from 'react'
-import styled from 'styled-components'
-import './styles.css'
+import React from "react";
+import styled from "styled-components";
+import "./styles.css";
 
 const TarefaList = styled.ul`
   padding: 0;
   width: 200px;
-`
+`;
 
 const Tarefa = styled.li`
   text-align: left;
-  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
-`
+  text-decoration: ${({ completa }) => (completa ? "line-through" : "none")};
+`;
 
 const InputsContainer = styled.div`
   display: grid;
   grid-auto-flow: column;
   gap: 10px;
-`
+`;
 
 class App extends React.Component {
-    state = {
-      tarefas: [],
-      inputValue: '',
-      filtro: ''
-    }
+  state = {
+    tarefas: [],
+    inputValue: "",
+    filtro: "",
+  };
 
   componentDidUpdate() {
+    localStorage.setItem("Nova tarefa", JSON.stringify(this.state.tarefas));
+  }
 
-  };
-
-  componentDidMount() {
-
-  };
+  componentDidMount() {}
 
   onChangeInput = (event) => {
-
-  }
+    this.setState({ inputValue: event.target.value });
+  };
 
   criaTarefa = () => {
+    if (this.state.inputValue !== "") {
+      const novasTarefas = [...this.state.tarefas];
 
-  }
+      novasTarefas.push({
+        id: Date.now(),
+        texto: this.state.inputValue,
+        completa: false,
+      });
+
+      console.log(novasTarefas);
+
+      this.setState({ tarefas: novasTarefas, inputValue: "" });
+
+      // localStorage.setItem("Novas tarefas", JSON.stringify(novasTarefas))
+      // const dados = JSON.stringify(novasTarefas)
+      // const dadosObjeto = JSON.parse(dados)
+      // console.log(dadosObjeto)
+    } else {
+      alert("Campo vazio");
+    }
+  };
 
   selectTarefa = (id) => {
+    const novaListaDetarefas = this.state.tarefas.map((tarefa) => {
+      if (id === tarefa.id) {
+        const novaTarefa = {
+          ...tarefa,
+          completa: !tarefa.completa,
+        };
+        return novaTarefa;
+      } else {
+        return tarefa;
+      }
+    });
 
-  }
+    this.setState({ tarefas: novaListaDetarefas });
+  };
 
   onChangeFilter = (event) => {
-
-  }
+    this.setState({ filtro: event.target.value });
+  };
 
   render() {
-    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+    const listaFiltrada = this.state.tarefas.filter((tarefa) => {
       switch (this.state.filtro) {
-        case 'pendentes':
-          return !tarefa.completa
-        case 'completas':
-          return tarefa.completa
+        case "pendentes":
+          return !tarefa.completa;
+        case "completas":
+          return tarefa.completa;
         default:
-          return true
+          return true;
       }
-    })
+    });
 
     return (
       <div className="App">
         <h1>Lista de tarefas</h1>
         <InputsContainer>
-          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <input value={this.state.inputValue} onChange={this.onChangeInput} />
+
           <button onClick={this.criaTarefa}>Adicionar</button>
         </InputsContainer>
-        <br/>
+        <br />
 
         <InputsContainer>
           <label>Filtro</label>
@@ -79,7 +109,7 @@ class App extends React.Component {
           </select>
         </InputsContainer>
         <TarefaList>
-          {listaFiltrada.map(tarefa => {
+          {listaFiltrada.map((tarefa) => {
             return (
               <Tarefa
                 completa={tarefa.completa}
@@ -87,12 +117,12 @@ class App extends React.Component {
               >
                 {tarefa.texto}
               </Tarefa>
-            )
+            );
           })}
         </TarefaList>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
